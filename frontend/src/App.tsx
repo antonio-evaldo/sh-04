7c173bbc24dc84220d38b8caed7e09f30bce100f
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-06-05 21:06:56 -0300

    feat: instala tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1 className="bg-gray-900 text-green-500">Hello World!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
